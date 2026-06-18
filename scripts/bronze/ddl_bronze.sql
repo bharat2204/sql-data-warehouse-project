f0354d5a7bc7b231e6929a75a203da0cb198e851
K V S Bharadwaj Reddy <reddykvsbharadwaj@gmail.com>
2026-06-18 16:25:00 +0530

    Add header comment to ddl_bronze.sql
    Added a comment header to the DDL script for clarity.

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,3 +1,8 @@
+/*
+*********************************************************************
+DDL Script: Create Bronze Tables
+*********************************************************************
+*/
 IF OBJECT_ID('bronze.crm_cust_info','U') IS NOT NULL
   DROP TABLE bronze.crm_cust_info;
  GO
